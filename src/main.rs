e477a6104305a22cb5ca477a08afcb6062886875
Felix <mrKaasa@protonmail.com>
2023-10-02 10:07:48 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![allow(unused)]
+
+use std::net::SocketAddr;
+use axum::response::Html;
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(
+            || async {
+                Html("Hello, world!")
+            }
+        )
+    );
+    let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
+    println!("->> LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+}
