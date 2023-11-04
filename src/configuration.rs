6906d871915bbefdd6db42191896ede9a40167b0
Felix <mrKaasa@protonmail.com>
2023-11-04 19:34:07 +0100

    cleaning up shuttle rs imports

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,21 +1,20 @@
 use secrecy::{ExposeSecret, Secret};
 use std::net::Ipv4Addr;
 use config::ConfigError;
-use shuttle_secrets::SecretStore;
 
 pub struct DatabaseSettings {
     pub host: String,
     pub secret_key: Secret<String>,
 }
-pub fn get_configuration(secrets: &SecretStore) -> Result<DatabaseSettings, config::ConfigError> {
-    let db_secret_key = if let Some(db_secret_key) = secrets.get("db_secret_key") {
-        db_secret_key
-    } else {return Err(config::ConfigError::NotFound(String::from("db_secret_key")))};
-    let db_host = if let Some(db_host) = secrets.get("db_host") {
-        db_host
-    } else {return Err(config::ConfigError::NotFound(String::from("db_host")))};
+pub fn get_configuration() -> Result<DatabaseSettings, ConfigError> {
+    // let db_secret_key = if let Some(db_secret_key) = secrets.get("db_secret_key") {
+    //     db_secret_key
+    // } else {return Err(config::ConfigError::NotFound(String::from("db_secret_key")))};
+    // let db_host = if let Some(db_host) = secrets.get("db_host") {
+    //     db_host
+    // } else {return Err(config::ConfigError::NotFound(String::from("db_host")))};
     Ok(DatabaseSettings {
-            host: db_host,
-            secret_key: Secret::new(db_secret_key),
+        secret_key: Secret::new("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhYWp2bXBlY3lnbGdpbGhkdGFuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4OTU4ODc3NiwiZXhwIjoyMDA1MTY0Nzc2fQ.hqRxE6YruUyUSJaTSNkgvKextnyK2VLj40oXmtT323U".into()),
+        host: "https://vaajvmpecyglgilhdtan.supabase.co/rest/v1".into()
     })
 }
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,5 +1,5 @@
 pub mod mw_auth;
-pub mod routes_pordcast;
+pub mod routes_podcast;
 pub mod routes_subscribe;
 
 pub const AUTH_TOKEN: &str = "next-auth.session-token";
